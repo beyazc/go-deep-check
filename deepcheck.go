856166299ca2f7b1cb130570fc94b830f694a63f
cardtek\cahit.beyaz <cahit.beyaz@cardtek.com>
2017-04-07 08:18:49 +0300

    primitice types tests added. but struct comparision is not added yet. so there is one failing test case

--- deepcheck.go
+++ deepcheck.go
@@ -2,6 +2,7 @@ package deepcheck
 
 import (
 	"errors"
+	"fmt"
 	"log"
 	"reflect"
 )
@@ -11,6 +12,10 @@ var (
 	ErrTypesDoesNotMatch = errors.New("variables type did not matched")
 	//ErrNilParameterReceived indicates that a compare parameter was nil
 	ErrNilParameterReceived = errors.New("A nil parameter recevied")
+	//ErrValueMismatched values does not match
+	ErrValueMismatched = errors.New("Values does not match")
+	//ErrNotHandled indicates that this comparision is not handled yet
+	ErrNotHandled = errors.New("comparision for type is not defined")
 )
 
 //Equals returns whether given interfaces are equal or not
@@ -37,6 +42,41 @@ func Equals(a, b interface{}) bool {
 
 	if b == nil && a != nil {
 		log.Println(ErrNilParameterReceived)
+		return false
+	}
+	aVal := reflect.ValueOf(a)
+	bVal := reflect.ValueOf(b)
+
+	switch aVal.Kind() {
+	//primitive types
+	case reflect.Float32, reflect.Float64:
+		if aVal.Float() != bVal.Float() {
+			log.Println(ErrValueMismatched)
+			return false
+		}
+	case reflect.Bool:
+		if aVal.Bool() != bVal.Bool() {
+			log.Println(ErrValueMismatched)
+			return false
+		}
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		if aVal.Int() != bVal.Int() {
+			log.Println(ErrValueMismatched)
+			return false
+		}
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+		if aVal.Uint() != bVal.Uint() {
+			log.Println(ErrValueMismatched)
+			return false
+		}
+	case reflect.String:
+		if aVal.String() != bVal.String() {
+			log.Println(ErrValueMismatched)
+			return false
+		}
+
+	default:
+		fmt.Println(ErrNotHandled)
 		return false
 	}
 
--- deepcheck_test.go
+++ deepcheck_test.go
@@ -17,12 +17,16 @@ func TestDifferentTypes(t *testing.T) {
 		t.Error("different types should not be equal")
 	}
 
-	if !Equals(student1, student2) {
+	if Equals(student1, student2) {
 		t.Error("same type should be equal")
 	}
 
 	if Equals(student1, &student1) {
 		t.Error("a type with its reference is also not same")
+	}
+
+	if !Equals(student1, student1) {
+		t.Error("same struct must be equal its equivelent")
 	}
 
 }
@@ -43,5 +47,78 @@ func TestNil(t *testing.T) {
 }
 
 func TestEqualsInterfacePointer(t *testing.T) {
+	number1 := 12
+	number2 := 13
+
+	if Equals(number1, number2) {
+		t.Error("different values should not be equal")
+	}
+
+	number2 = 12
+	if !Equals(number1, number2) {
+		t.Error("same values should be equal")
+	}
+
+}
+
+func TestPrimitives(t *testing.T) {
+	var intVal1 int = 2
+	var intVal2 int = 2
+	var intval3 int = 3
+
+	if !Equals(intVal1, intVal2) {
+		t.Error("same int values shoul be equal")
+	}
+
+	if Equals(intVal1, intval3) {
+		t.Error("different int values shoul not be equal")
+	}
+
+	var floatVal1 float32 = 2.0
+	var floatVal2 float32 = 2.0
+	var floatVal3 float32 = 3
+
+	if !Equals(floatVal1, floatVal2) {
+		t.Error("same float values shoul be equal")
+	}
+
+	if Equals(floatVal1, floatVal3) {
+		t.Error("different float values shoul not be equal")
+	}
+
+	var boolVal1 bool = true
+	var boolVal2 bool = true
+	var boolVal3 bool = false
+
+	if !Equals(boolVal1, boolVal2) {
+		t.Error("same bool values shoul be equal")
+	}
+
+	if Equals(boolVal1, boolVal3) {
+		t.Error("different bool values shoul not be equal")
+	}
+
+	var stringVal1 string = "deepcheck"
+	var stringVal2 string = "deepcheck"
+	var stringVal3 string = "checdeep"
+
+	if !Equals(stringVal1, stringVal2) {
+		t.Error("same bool string shoul be equal")
+	}
+
+	if Equals(stringVal1, stringVal3) {
+		t.Error("different string values shoul not be equal")
+	}
+
+	var uintVal1 uint = 1
+	var uintVal2 uint = 1
+	var uintVal3 uint = 2
+	if !Equals(uintVal1, uintVal2) {
+		t.Error("same bool uint shoul be equal")
+	}
+
+	if Equals(uintVal1, uintVal3) {
+		t.Error("different uint values shoul not be equal")
+	}
 
 }
